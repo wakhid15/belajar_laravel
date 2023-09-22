5acd2aaefd1d6cdb723ccb13bf4b0d80551e3888
wakhid15 <95697443+wakhid15@users.noreply.github.com>
2023-09-22 17:29:07 +0700

    Initial commit

---
+++ resources/views/ludien.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portofolio Saya</title>
+  
+  <!-- Tambahkan Bootstrap 5 CSS dari CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>Selamat Datang di Portofolio Saya</h1>
+        <p>Ini adalah portofolio sederhana menggunakan Bootstrap 5.</p>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <h2>Pengalaman Kerja</h2>
+        <ul>
+          <li>Posisi: Mahasiswa</li>
+          <li>Tahun: 2021 - Sekarang</li>
+          <li>Deskripsi: Belajar bertanggung jawab untuk pengembangan situs web perusahaan dan pemeliharaan situs yang sudah ada.</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <h2>Pendidikan</h2>
+        <ul>
+          <li>Prodi: D3 Sistem Informasi</li>
+          <li>Universitas: STIMATA MALANG</li>
+          <li>Tahun: Kemarin - Sekarang</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <h2>Keterampilan</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Bootstrap 5</li>
+          <li>PHP</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- Tambahkan Bootstrap 5 JavaScript dari CDN (Popper.js dan Bootstrap.js diperlukan) -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
